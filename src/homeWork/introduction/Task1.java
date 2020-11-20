24ae63bc2759abbee71c2fcd5d1e02e3cc35cec8
maxgrovs <maxgrovs@gmail.com>
2020-11-20 23:47:11 +0300

    task_0

--- src/homeWork/introduction/Task1.java
+++ src/homeWork/introduction/Task1.java
@@ -1,12 +1,9 @@
-package homeWork;
+package homeWork.introduction;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
---
+++ src/homeWork/task_0/app/Main.java
@@ -0,0 +1,42 @@
+package homeWork.task_0.app;
+
+import homeWork.task_0.model.Point;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    static Logger logger = LogManager.getLogger("main");
+
+    public static void main(String[] args) {
+
+        Point[] points = collectPoint(new Point(3, 7), new Point(0, 10));
+
+        writeLog(points);
+
+    }
+
+    public static Point[] collectPoint(Point... input) {
+        Point[] points;
+        points = new Point[input.length];
+
+        int i = 0;
+        for (Point point : input) {
+            points[i] = point;
+            i++;
+        }
+
+        return points;
+    }
+
+    public static void writeLog(Point[] points) {
+
+        for (Point point :
+                points) {
+            logger.info(point.toString());
+        }
+
+    }
+
+
+}
