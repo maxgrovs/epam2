69e96a1ffa5987b84fa71b74fc20d619a1d0f276
max <maxgrovs@gmail.com>
2020-11-27 11:30:04 +0300

    task_1 61

---
+++ src/homeWork/task_1/logic/view/concreteView/SquareView.java
@@ -0,0 +1,24 @@
+package homeWork.task_1.logic.view.concreteView;
+
+import homeWork.task_1.logic.view.ShapeView;
+import homeWork.task_1.model.Shape;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SquareView implements ShapeView {
+
+    private static Logger logger = LogManager.getLogger(SquareView.class);
+
+
+    @Override
+    public void print(Shape[] shapes) {
+
+        for (Shape shape :
+                shapes) {
+            logger.info(shape.toString());
+        }
+
+
+    }
+
+}
