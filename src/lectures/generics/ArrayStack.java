89da6c6f8aaf703a2e64c691ee035a617f5381f3
max <maxgrovs@gmail.com>
2020-12-24 14:35:08 +0300

    generics

--- src/lectures/generics/ArrayStack.java
+++ src/lectures/generics/ArrayStack.java
@@ -4,14 +4,23 @@ import java.util.Collection;
 
 public class ArrayStack<E> implements Stack<E> {
 
-    private Object [] objects;
+    private Object[] objects;
     private int capacity;
     private int size;
 
+    public ArrayStack(int capacity) {
+        this.capacity = capacity;
+        size = 0;
+        objects = new Object[capacity];
+    }
 
     @Override
     public void push(E element) throws StackExeption {
-
+        if (isFull()) {
+            throw new StackExeption("Stack overflow");
+        }
+        objects[size] = element;
+        size++;
     }
 
     @Override
--- src/lectures/generics/ArraysRunner.java
+++ src/lectures/generics/ArraysRunner.java
@@ -2,7 +2,14 @@ package lectures.generics;
 
 public class ArraysRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws StackExeption {
+
+        ArrayStack<Integer> ints = new ArrayStack<>(20);
+
+        ints.push(10);
+        ints.push(29);
+
+
 
     }
 
